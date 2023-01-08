e665a1143aa7c61c9bbc333a10368ceda2ea737b
yeomjinseop <yeomjinseop@gmail.com>
2023-01-08 09:04:49 +0900

    manage array state

---
+++ simpleExample/basic-mentors/.history/src/AppMentors_20230107215430.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: '엘리',
+    title: '개발자',
+    mentors: [
+      {
+        name: '밥',
+        title: '시니어개발자',
+      },
+      {
+        name: '제임스',
+        title: '시니어개발자',
+      },
+    ],
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>{person.name}의 멘토는:</p>
+      <ul>
+        {person.mentors.map((mentor, index) => (
+          <li key={index}>
+            {mentor.name} ({mentor.title})
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+          const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+          setPerson((prev) => ({...prev, mentors: prev.mentors.map((mentor) => 
+           mentor.name += '🍎'
+          )}))
+        }}
+      >
+        멘토의 이름을 바꾸기
+      </button>
+    </div>
+  );
+}
