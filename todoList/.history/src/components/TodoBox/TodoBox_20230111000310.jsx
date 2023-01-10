479778a689bf95be3b786d63bb7e45cd44279ad6
yeomjinseop <yeomjinseop@gmail.com>
2023-01-11 00:16:22 +0900

    create default component

--- todoList/.history/src/components/TodoBox/TodoBox_20230111000310.jsx
+++ todoList/.history/src/components/TodoBox/TodoBox_20230111000310.jsx
@@ -19,11 +19,9 @@ function TodoBox(props) {
         <Todo/>
         <Todo/>
       </div>
-      <footer className={styles.footer}>
-        <div className={styles.todoInput}>
-          <input type="text" />
-          <button>add</button>
-        </div>
+      <footer>
+        <input type="text" />
+        <input type="submit" />
       </footer>
     </div>
   );
--- todoList/.history/src/components/TodoBox/TodoBox_20230111000432.jsx
+++ todoList/.history/src/components/TodoBox/TodoBox_20230111000432.jsx
@@ -19,11 +19,9 @@ function TodoBox(props) {
         <Todo/>
         <Todo/>
       </div>
-      <footer className={styles.footer}>
-        <div className={styles.todoInput}>
-          <input type="text" />
-          <button>add</button>
-        </div>
+      <footer className={styles.todoInput}>
+        <input type="text" />
+        <input type="submit" value='add'/>
       </footer>
     </div>
   );
